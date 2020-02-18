35b488afe67defd17bcc6392ef01709fe854ea38
aburkleaux-amazon <33331299+aburkleaux-amazon@users.noreply.github.com>
2020-02-18 09:11:30 -0800

    Create component based orgainzation for project source. Add build script to create  cloudfromation that can be deployed from andy region. Add build script to package lambdas Add copilot component that uses a lambda@edge to control the http response of a video stream that is fronted by CloudFront

---
+++ source/cfn-add-edge-lambda/cfn-add-edge-lambda.py
@@ -0,0 +1,128 @@
+from crhelper import CfnResource
+import boto3
+import logging
+import json
+import copy
+
+logger = logging.getLogger(__name__)
+# Initialise the helper, all inputs are optional, this example shows the defaults
+helper = CfnResource(json_logging=False, log_level='DEBUG', boto_level='CRITICAL')
+
+try:
+    ## Init code goes here
+    pass
+except Exception as e:
+    helper.init_failure(e)
+    
+client = boto3.client('cloudfront')
+
+def handler(event, context):
+    helper(event, context)
+
+@helper.create
+@helper.update
+def create(event, context):
+    logger.info("Got Create")
+    logger.info(json.dumps(event))
+    # Optionally return an ID that will be used for the resource PhysicalResourceId, 
+    # if None is returned an ID will be generated. If a poll_create function is defined 
+    # return value is placed into the poll event as event['CrHelperData']['PhysicalResourceId']
+    #
+
+    # Check that all the required properties are specified
+    if "Id" not in event["ResourceProperties"]:
+        raise ValueError("Missing property 'Id'")
+    if "LambdaFunctionARN" not in event["ResourceProperties"]:
+        raise ValueError("Missing property 'LambdaFunctionARN'")
+    if "EventType" not in event["ResourceProperties"]:
+        raise ValueError("Missing property 'EventType'")
+
+    new_lambda_association = {
+            "LambdaFunctionARN": event["ResourceProperties"]["LambdaFunctionARN"],
+            "EventType": event["ResourceProperties"]["EventType"],
+            "IncludeBody": False
+        }
+    
+    try:
+
+        response = client.get_distribution_config(Id=event["ResourceProperties"]["Id"])
+
+        config = copy.deepcopy(response["DistributionConfig"]) 
+        saved_config = copy.deepcopy(response["DistributionConfig"]) 
+        
+
+        config["DefaultCacheBehavior"]["LambdaFunctionAssociations"]["Quantity"] = config["DefaultCacheBehavior"]["LambdaFunctionAssociations"]["Quantity"] + 1
+        if "Items" not in config["DefaultCacheBehavior"]["LambdaFunctionAssociations"]:
+            config["DefaultCacheBehavior"]["LambdaFunctionAssociations"]["Items"] = []
+        
+        config["DefaultCacheBehavior"]["LambdaFunctionAssociations"]["Items"].append(new_lambda_association)
+
+        response = client.update_distribution(DistributionConfig=config, Id=event["ResourceProperties"]["Id"], IfMatch=response["ETag"])
+    except client.exceptions.InvalidLambdaFunctionAssociation as e:
+
+        logger.info("Got InvalidLambdaFunctionAssociation...check if this lambda behavior already exists")
+        
+        # Check if this exact lambda association already exists and return success if it does.  This
+        # protects us from deleting an association we didn't create in the Cloudformation error
+        # path which will call helper.delete with this event
+        matches = []
+        if "Items" in saved_config["DefaultCacheBehavior"]["LambdaFunctionAssociations"]:
+            matches = [x for x in saved_config["DefaultCacheBehavior"]["LambdaFunctionAssociations"]["Items"] if x == new_lambda_association]
+        
+        if len(matches) > 0:
+            logger.info("this lambda behavior already exists")
+        else:
+            logger.info("this lambda behavior does not already exist")
+            raise e
+
+    except Exception as e:
+        raise e
+    
+    return "MyResourceId"
+
+@helper.delete
+def delete(event, context):
+    logger.info("Got Delete")
+    # Delete never returns anything. Should not fail if the underlying resources are already deleted.
+    # Desired state.
+
+    # Check that all the required properties are specified
+    if "Id" not in event["ResourceProperties"]:
+        raise ValueError("Missing property 'Id'")
+    if "LambdaFunctionARN" not in event["ResourceProperties"]:
+        raise ValueError("Missing property 'LambdaFunctionARN'")
+    if "EventType" not in event["ResourceProperties"]:
+        raise ValueError("Missing property 'EventType'")
+
+    lambda_association = {
+            "LambdaFunctionARN": event["ResourceProperties"]["LambdaFunctionARN"],
+            "EventType": event["ResourceProperties"]["EventType"],
+            "IncludeBody": False
+        }
+
+    # Delete the associated lambda function behavior if it exists for the distribution
+    try:
+
+        response = client.get_distribution_config(Id=event["ResourceProperties"]["Id"])
+
+        config = response["DistributionConfig"]  
+        
+        # Check if this exact lambda association already exists and return success if it does.  This
+        # protects us from deleting an association we didn't create in the Cloudformation error
+        # path which will call helper.delete with this event
+        matches = []
+        if "Items" in config["DefaultCacheBehavior"]["LambdaFunctionAssociations"]:
+            matches = [x for x in config["DefaultCacheBehavior"]["LambdaFunctionAssociations"]["Items"] if x == lambda_association]
+        
+        
+        if len(matches) > 0:
+            logger.info("Found matching LambdaFunctionAssociation to remove from the CloudFront Distribution")
+            config["DefaultCacheBehavior"]["LambdaFunctionAssociations"]["Items"].remove(lambda_association)
+            config["DefaultCacheBehavior"]["LambdaFunctionAssociations"]["Quantity"] = len(config["DefaultCacheBehavior"]["LambdaFunctionAssociations"]["Items"])
+            response = client.update_distribution(DistributionConfig=config, Id=event["ResourceProperties"]["Id"], IfMatch=response["ETag"])
+
+    except Exception as e:
+        raise e
+    
+
+
---
+++ source/cfn-global-table/cfn-global-table.py
@@ -0,0 +1,162 @@
+# Adapted from: https://github.com/kspurrier/cfn-dynamodb-global-table/blob/master/DynamoDBGlobalTableCreate/dynamoDBGlobalTableCreate.py
+
+import boto3
+import cfnresponse
+import json
+import logging
+import os
+
+logger = logging.getLogger()
+
+if 'LoggingLevel' in os.environ.keys() and os.environ['LoggingLevel']:
+   log_level = logging.getLevelName(os.environ['LoggingLevel'])
+else:
+   log_level = logging.getLevelName('WARNING')
+
+log_handler = logger.handlers[0]
+
+if log_level == 30:
+   log_handler.setFormatter(logging.Formatter('%(message)s'))
+else:
+   log_handler.setFormatter(logging.Formatter('[%(levelname)s]: %(message)s'))
+
+logger.setLevel(log_level)
+
+
+def global_table_create(event, context):
+    logger.info(json.dumps(event))
+    global_table_name = event['ResourceProperties']['GlobalTableName']
+
+    try:
+        resp = {'Status': 'FAILED', 'Data': {'GlobalTableName': global_table_name, 'LogGroup': context.log_group_name }}
+        client = boto3.client('dynamodb')
+
+        logger.debug('Creating DynamoDB Global Table ' + global_table_name)
+        response = {}
+        replication_group = []
+
+        for region in event['ResourceProperties']['ReplicationGroupList']:
+            replication_group.append({ 'RegionName': region})
+
+        response = client.create_global_table(
+            GlobalTableName=global_table_name,
+            ReplicationGroup=replication_group
+        )
+        logger.info(response)
+        resp['Reason'] = 'Created Global Table: ' + global_table_name
+        resp['Status'] = 'SUCCESS'
+
+    except client.exceptions.GlobalTableAlreadyExistsException as e:
+        logger.info('Global Table already exists, falling back to update logic: ' + e.message )
+        # Describe the already existing global table, so we can attempt to update instead
+        response = {}
+        response = client.describe_global_table(
+            GlobalTableName=global_table_name
+        )
+
+        logger.debug(response)
+
+        existing_replication_group_list = []
+        for region in response['GlobalTableDescription']['ReplicationGroup']:
+            logger.debug('Existing Replication Group List')
+            existing_replication_group_list.append(region['RegionName'])
+        event['OldResourceProperties']= { 'ReplicationGroupList': existing_replication_group_list }
+        response = global_table_update(event,context)
+        return response
+
+    except Exception as e:
+        logger.error('Failed to create DynamoDB Global Table: ' + global_table_name)
+        resp['Reason'] = 'Failed to create DynamoDB Global Table: ' + e.message 
+        resp['Status'] = 'FAILED'
+    
+    return resp
+
+def global_table_update(event, context):
+    logger.debug(json.dumps(event))
+    global_table_name = event['ResourceProperties']['GlobalTableName']
+
+    try:
+        resp = {'Status': 'FAILED', 'Data': {'GlobalTableName': global_table_name, 'LogGroup': context.log_group_name }}
+        client = boto3.client('dynamodb')
+
+        # Describe the existing global table, as we cannot trust the provided
+        # event['OldResourceProperties']['ReplicationGroupList']) to show the
+        # current actual replication group list for a stack update failure / rollback.
+        response = {}
+        response = client.describe_global_table(
+            GlobalTableName=global_table_name
+        )
+        logger.debug(response)
+
+        existing_replication_groups = set() 
+        for region in response['GlobalTableDescription']['ReplicationGroup']:
+            existing_replication_groups.add(region['RegionName'])
+
+        new_rep_groups = set(event['ResourceProperties']['ReplicationGroupList'])
+        old_rep_groups = set(event['OldResourceProperties']['ReplicationGroupList'])
+
+        changeset_rep_group_discrepancies = old_rep_groups - existing_replication_groups
+        if len(changeset_rep_group_discrepancies):
+            logger.info("Using the results of describe_global_table rather than the value from OldResourceProperties in the event")
+            # Use reality, not the value from OldResourceProperties in the event
+            old_rep_groups = existing_replication_groups
+
+        if new_rep_groups == old_rep_groups:
+            logger.info('No replication group changes detected')
+            resp['Reason'] = 'No replication group changes detected'
+            resp['Status'] = 'SUCCESS'
+            return resp
+        else:
+            logger.info('Replication group changes detected. Updating Global Table')
+
+        AddReplicationGroups = new_rep_groups - old_rep_groups
+        RemoveReplicationGroups = old_rep_groups - new_rep_groups
+
+        if len(AddReplicationGroups):
+            for region in AddReplicationGroups:
+                update_global_table(global_table_name, region, 'Create')
+                logger.info('Add replication group region: ' + region)
+
+        if len(RemoveReplicationGroups):
+            for region in RemoveReplicationGroups:
+                update_global_table(global_table_name, region, 'Delete')
+                logger.info('Remove replication group region: ' + region)
+
+        resp['Reason'] = 'Updated Global Table: ' + global_table_name
+        resp['Status'] = 'SUCCESS'
+    except Exception as e:
+        logger.error('Failed to update DynamoDB Global Table: ' + global_table_name)
+        resp['Reason'] = 'Failed to update Global Table: ' + e.message
+        resp['Status'] = 'FAILED'
+    
+    return resp
+
+def update_global_table(global_table_name, region, action):
+    client = boto3.client('dynamodb')
+    ReplicaUpdates = []
+    ReplicaUpdate = {}
+    ReplicationGroup = {}
+    ReplicationGroup['RegionName'] = region
+    ReplicaUpdate[action] = ReplicationGroup
+    ReplicaUpdates.append(ReplicaUpdate)
+
+    response = client.update_global_table(
+        GlobalTableName=global_table_name,
+        ReplicaUpdates=ReplicaUpdates
+    )
+
+    return response
+
+
+def handler(event, context):
+  if event['RequestType'] == 'Create':
+    response = global_table_create(event,context)
+  if event['RequestType'] == 'Delete':
+    response = {'Status': 'SUCCESS', 'Data': { 'LogGroup': context.log_group_name }}
+  if event['RequestType'] == 'Update':
+    response = global_table_update(event,context)
+
+  if 'Reason' in response.keys():
+    cfnresponse.send(event, context, log_level, response['Status'], response['Data'], response['Reason'])
+  else:
+    cfnresponse.send(event, context, log_level, response['Status'], response['Data'])
