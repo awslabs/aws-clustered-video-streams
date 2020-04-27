e6f16d1566b582b98cde5caf96a8a6575941129a
Jim Thario <jtthario@amazon.com>
2020-04-27 16:59:49 -0700

    Add L@E to all CloudFront behaviors except those with SmoothStreaming enabled #24

--- source/cfn-add-edge-lambda/cfn-add-edge-lambda.py
+++ source/cfn-add-edge-lambda/cfn-add-edge-lambda.py
@@ -9,18 +9,66 @@ import copy
 
 logger = logging.getLogger(__name__)
 # Initialise the helper, all inputs are optional, this example shows the defaults
-helper = CfnResource(json_logging=False, log_level='DEBUG', boto_level='CRITICAL')
+helper = CfnResource(json_logging=False,
+                     log_level='DEBUG',
+                     boto_level='CRITICAL')
 
 try:
     ## Init code goes here
     pass
 except Exception as e:
     helper.init_failure(e)
-    
+
 client = boto3.client('cloudfront')
+
+
+def update_cache_behavior(behavior, lambda_association):
+    # only proceed if this is not a smooth streaming behavior
+    if not behavior.get("SmoothStreaming", False):
+        # default to an empty list if none yet
+        behavior["LambdaFunctionAssociations"]["Items"] = behavior[
+            "LambdaFunctionAssociations"].get("Items", [])
+        # look for existing associations
+        matches = [
+            x for x in behavior["LambdaFunctionAssociations"]["Items"]
+            if x["EventType"] == lambda_association["EventType"]
+        ]
+        if len(matches) > 0:
+            # replace the lambda arn with the new lambda
+            matches[0]["LambdaFunctionARN"] = lambda_association[
+                "LambdaFunctionARN"]
+            matches[0]["IncludeBody"] = lambda_association["IncludeBody"]
+        else:
+            # create a new association
+            behavior["LambdaFunctionAssociations"]["Items"].append(
+                lambda_association)
+            behavior["LambdaFunctionAssociations"]["Quantity"] = behavior[
+                "LambdaFunctionAssociations"]["Quantity"] + 1
+
+
+def remove_cache_behavior(behavior, lambda_association):
+    # default to an empty list if none yet
+    behavior["LambdaFunctionAssociations"]["Items"] = behavior[
+        "LambdaFunctionAssociations"].get("Items", [])
+    # look for existing associations
+    matches = [
+        x for x in behavior["LambdaFunctionAssociations"]["Items"]
+        if x == lambda_association
+    ]
+
+    if len(matches) > 0:
+        logger.info(
+            "Found matching LambdaFunctionAssociation to remove from the CloudFront Distribution"
+        )
+        behavior["LambdaFunctionAssociations"]["Items"].remove(
+            lambda_association)
+        behavior["LambdaFunctionAssociations"]["Quantity"] = len(
+            behavior["LambdaFunctionAssociations"]["Items"])
+
 
 def handler(event, context):
     helper(event, context)
+
 
 @helper.create
 @helper.update
@@ -28,10 +76,6 @@ def create(event, context):
     ReturnId = 0
     logger.info("Got Create")
     logger.info(json.dumps(event))
-    # Optionally return an ID that will be used for the resource PhysicalResourceId, 
-    # if None is returned an ID will be generated. If a poll_create function is defined 
-    # return value is placed into the poll event as event['CrHelperData']['PhysicalResourceId']
-    #
 
     # Check that all the required properties are specified
     if "Id" not in event["ResourceProperties"]:
@@ -41,57 +85,40 @@ def create(event, context):
     if "EventType" not in event["ResourceProperties"]:
         raise ValueError("Missing property 'EventType'")
 
-    ReturnId = event["ResourceProperties"]["Id"]
+    # ReturnId = event["ResourceProperties"]["Id"]
 
-    new_lambda_association = {
-            "LambdaFunctionARN": event["ResourceProperties"]["LambdaFunctionARN"],
-            "EventType": event["ResourceProperties"]["EventType"],
-            "IncludeBody": False
-        }
-    
+    lambda_association = {
+        "LambdaFunctionARN": event["ResourceProperties"]["LambdaFunctionARN"],
+        "EventType": event["ResourceProperties"]["EventType"],
+        "IncludeBody": False
+    }
+
     try:
+        response = client.get_distribution_config(
+            Id=event["ResourceProperties"]["Id"])
 
-        response = client.get_distribution_config(Id=event["ResourceProperties"]["Id"])
+        config = copy.deepcopy(response["DistributionConfig"])
+        saved_config = copy.deepcopy(response["DistributionConfig"])
 
-        config = copy.deepcopy(response["DistributionConfig"]) 
-        saved_config = copy.deepcopy(response["DistributionConfig"]) 
-        
-        # If no lambda associations exist create the lambda associations list
-        if "Items" not in config["DefaultCacheBehavior"]["LambdaFunctionAssociations"]:
-            config["DefaultCacheBehavior"]["LambdaFunctionAssociations"]["Items"] = []
-        
-        # If there is an existing association for this event type, replace the lambda arn with the new lambda, otherwise create a new association
-        matches = []
-        matches = [x for x in config["DefaultCacheBehavior"]["LambdaFunctionAssociations"]["Items"] if x["EventType"] == new_lambda_association["EventType"]]
-        if len(matches) > 0:
-            matches[0]["LambdaFunctionARN"] = new_lambda_association["LambdaFunctionARN"]
-            matches[0]["IncludeBody"] = new_lambda_association["IncludeBody"]
-        else: 
-            config["DefaultCacheBehavior"]["LambdaFunctionAssociations"]["Items"].append(new_lambda_association)
-            config["DefaultCacheBehavior"]["LambdaFunctionAssociations"]["Quantity"] = config["DefaultCacheBehavior"]["LambdaFunctionAssociations"]["Quantity"] + 1
+        # handle the DefaultCacheBehavior key first
+        update_cache_behavior(config["DefaultCacheBehavior"],
+                              lambda_association)
 
-        response = client.update_distribution(DistributionConfig=config, Id=event["ResourceProperties"]["Id"], IfMatch=response["ETag"])
-    # except client.exceptions.InvalidLambdaFunctionAssociation as e:
+        # handle cache behaviors
+        for behavior in config["CacheBehaviors"].get("Items", []):
+            update_cache_behavior(behavior, lambda_association)
 
-    #     logger.info("Got InvalidLambdaFunctionAssociation...check if this lambda behavior already exists")
-        
-    #     # Check if this exact lambda association already exists and return success if it does.  This
-    #     # protects us from deleting an association we didn't create in the Cloudformation error
-    #     # path which will call helper.delete with this event
-    #     matches = []
-    #     if "Items" in saved_config["DefaultCacheBehavior"]["LambdaFunctionAssociations"]:
-    #         matches = [x for x in saved_config["DefaultCacheBehavior"]["LambdaFunctionAssociations"]["Items"] if x == new_lambda_association]
-        
-    #     if len(matches) > 0:
-    #         logger.info("this lambda behavior already exists")
-    #     else:
-    #         logger.info("this lambda behavior does not already exist")
-    #         raise e
+        # update CloudFormation and wait for the response
+        response = client.update_distribution(
+            DistributionConfig=config,
+            Id=event["ResourceProperties"]["Id"],
+            IfMatch=response["ETag"])
 
     except Exception as e:
         raise e
-    
-    return ReturnId
+
+    # return ReturnId
+
 
 @helper.delete
 def delete(event, context):
@@ -108,34 +135,30 @@ def delete(event, context):
         raise ValueError("Missing property 'EventType'")
 
     lambda_association = {
-            "LambdaFunctionARN": event["ResourceProperties"]["LambdaFunctionARN"],
-            "EventType": event["ResourceProperties"]["EventType"],
-            "IncludeBody": False
-        }
+        "LambdaFunctionARN": event["ResourceProperties"]["LambdaFunctionARN"],
+        "EventType": event["ResourceProperties"]["EventType"],
+        "IncludeBody": False
+    }
 
-    # Delete the associated lambda function behavior if it exists for the distribution
+    # delete any associated L@E functions with our Lambda ARN
     try:
+        # get the distribution
+        response = client.get_distribution_config(
+            Id=event["ResourceProperties"]["Id"])
+        config = response["DistributionConfig"]
 
-        response = client.get_distribution_config(Id=event["ResourceProperties"]["Id"])
+        # handle the DefaultCacheBehavior key first
+        remove_cache_behavior(config["DefaultCacheBehavior"],
+                              lambda_association)
 
-        config = response["DistributionConfig"]  
-        
-        # Check if this exact lambda association already exists and return success if it does.  This
-        # protects us from deleting an association we didn't create in the Cloudformation error
-        # path which will call helper.delete with this event
-        matches = []
-        if "Items" in config["DefaultCacheBehavior"]["LambdaFunctionAssociations"]:
-            matches = [x for x in config["DefaultCacheBehavior"]["LambdaFunctionAssociations"]["Items"] if x == lambda_association]
-        
-        
-        if len(matches) > 0:
-            logger.info("Found matching LambdaFunctionAssociation to remove from the CloudFront Distribution")
-            config["DefaultCacheBehavior"]["LambdaFunctionAssociations"]["Items"].remove(lambda_association)
-            config["DefaultCacheBehavior"]["LambdaFunctionAssociations"]["Quantity"] = len(config["DefaultCacheBehavior"]["LambdaFunctionAssociations"]["Items"])
-            response = client.update_distribution(DistributionConfig=config, Id=event["ResourceProperties"]["Id"], IfMatch=response["ETag"])
+        # handle cache behaviors
+        for behavior in config["CacheBehaviors"].get("Items", []):
+            remove_cache_behavior(behavior, lambda_association)
+
+        # update the distribution with changes
+        client.update_distribution(DistributionConfig=config,
+                                   Id=event["ResourceProperties"]["Id"],
+                                   IfMatch=response["ETag"])
 
     except Exception as e:
         raise e
-    
-
-
